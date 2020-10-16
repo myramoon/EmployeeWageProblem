0390689bd961878809ea8ba3d77251ea3196842b
anamfazal <anamfazal94@gmail.com>
2020-10-16 16:50:19 +0530

    Refactored: code to compute wages till 100 working hours or total working days is reached

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -1,4 +1,4 @@
-/* PURPOSE: Calculates wages for a month */
+/* PURPOSE: Calculates wages till either maximum working hours or days is raeched for a month */
 
 public class EmployeeWageBuilder {
 	
@@ -7,36 +7,42 @@ public class EmployeeWageBuilder {
 	static final int IS_PART_TIME = 1;  
 	static final int EMP_RATE_PER_HOUR = 20;
 	static final int MAX_WORKING_DAYS = 20;
+	static final int MAX_MONTHLY_HOURS = 100;
 
 	public static void main(String[] args) {
-
+	
 		//VARIABLES
 		int workHours = 0;
-		int employeeWage = 0;
-		int totalEmpWage = 0;
-
-		for(int day = 0; day < MAX_WORKING_DAYS; day++) {
-
-			int attendanceStatus=(int) Math.floor(Math.random() * 10) % 3; //Get attendance 
-			switch (attendanceStatus) {		   	              
+		int totalWorkHours = 0;
+		int totalWorkingDays = 0;
+		
+		//while loop to calculate work hours for each day until either 100hours are done or 20 days are reached
+		while (totalWorkHours <= MAX_MONTHLY_HOURS && totalWorkingDays < MAX_WORKING_DAYS) {
+	
+			totalWorkingDays++;					       //Increment working days counter
+			int attendanceStatus = (int) Math.floor(Math.random() * 10) % 3; //Get attendance 
+	
+			switch (attendanceStatus) {		  	                 //Condition check for 0 or 1
+	
 				case IS_FULL_TIME:
 					workHours = 8;
 					break;
 				case IS_PART_TIME:
 					workHours = 4;
 					break;
-				default:
+		    		default:
 					workHours = 0;
+	
 			}
-		
-			employeeWage = workHours * EMP_RATE_PER_HOUR; 			//wage calculation
-			totalEmpWage += employeeWage;  				        //sum of daily wages upto current day
+	
+			totalWorkHours += workHours; 					//total working hours logged upto current day
+			System.out.println("Day number: " + totalWorkingDays + "  Emp Hr: "+ workHours);    
 		
 		}
-
-		System.out.println("Employee wage for the month is : " + totalEmpWage);  //Print wage total after 20 days
+	
+		int totalEmpWage = totalWorkHours * EMP_RATE_PER_HOUR; 			//Calculate month's wages
+		System.out.println("Total employee wage for the month : " + totalEmpWage);  //Print final monthly wage 
 	}
 
 }
-
 
