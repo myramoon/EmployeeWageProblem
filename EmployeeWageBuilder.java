3016e6649d34c51a1202dfafe43c59322d662a35
anamfazal <anamfazal94@gmail.com>
2020-10-16 16:32:39 +0530

    Refactored: code to compute daily wage using switch case

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -1,26 +1,31 @@
-/* PURPOSE: Adds part time employee and wage */
+/* PURPOSE: Computes daily wage using switch case statement */
+
 public class EmployeeWageBuilder {
 	
 	//CONSTANTS
-	static final int IS_FULL_TIME=2;
-	static final int IS_PART_TIME=1;  
-	static final int EMP_RATE_PER_HOUR=20;
-
-	public static void main(String[] args) {
+	static final int IS_FULL_TIME = 2;
+	static final int IS_PART_TIME = 1;  
+	static final int EMP_RATE_PER_HOUR = 20;
 	
-		//VARIABLES
-		int workHours;
-		int employeeWage;
-		int attendanceStatus=(int) Math.floor(Math.random() * 10) % 3;  //Check attendance 
+	public static void main(String[] args) {
 
-		if (attendanceStatus == IS_FULL_TIME)		               
-			workHours = 8;
-		else if (attendanceStatus == IS_PART_TIME)
-			workHours = 4;
-		else
-			workHours = 0;
+		//VARIABLES
+        	int workHours;
+        	int employeeWage;
 		
-		employeeWage = workHours * EMP_RATE_PER_HOUR; 			//Wage calculation
+		int attendanceStatus = (int) Math.floor(Math.random() * 10) % 3; //Get attendance 
+		switch (attendanceStatus) {		               
+			case IS_FULL_TIME:
+ 				workHours = 8;
+				break;
+			case IS_PART_TIME:
+				workHours = 4;
+				break;
+		    	default:
+				workHours = 0;
+		}
+	
+		employeeWage = workHours * EMP_RATE_PER_HOUR; 		        //wage calculation
 		System.out.println("Employee wage for the day is : " + employeeWage);
 	}
 
