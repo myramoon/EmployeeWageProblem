54951c41623929dc6ce4acd25bc17980f518957d
anamfazal <anamfazal94@gmail.com>
2020-10-16 16:23:01 +0530

    Refactored: code to include part time condition

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -1,25 +1,27 @@
-/* Purpose: To compute employee's daily wage */
+/* PURPOSE: Adds part time employee and wage */
 public class EmployeeWageBuilder {
 	
 	//CONSTANTS
-	static final int IS_FULL_TIME=1;  
+	static final int IS_FULL_TIME=2;
+	static final int IS_PART_TIME=1;  
 	static final int EMP_RATE_PER_HOUR=20;
-	
-	public static void main(String[] args) {
 
+	public static void main(String[] args) {
+	
 		//VARIABLES
 		int workHours;
 		int employeeWage;
-		int attendanceStatus = (int) Math.floor(Math.random() * 10) % 2; //Get attendance as 0 or 1 for absent and present respectively
+		int attendanceStatus=(int) Math.floor(Math.random() * 10) % 3;  //Check attendance 
 
-		if (attendanceStatus == IS_FULL_TIME)			         //Condition check for 0 or 1
+		if (attendanceStatus == IS_FULL_TIME)		               
 			workHours = 8;
+		else if (attendanceStatus == IS_PART_TIME)
+			workHours = 4;
 		else
 			workHours = 0;
-
-		employeeWage = workHours * EMP_RATE_PER_HOUR; 			//wage calculation
+		
+		employeeWage = workHours * EMP_RATE_PER_HOUR; 			//Wage calculation
 		System.out.println("Employee wage for the day is : " + employeeWage);
-
 	}
 
 }
